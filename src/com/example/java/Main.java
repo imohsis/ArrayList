6c39e95139716538fdd7b486285d6c0fee82cd2c
simon <imohsis@gmail.com>
2018-01-06 12:43:09 +0000

    first commit on this project

---
+++ src/com/example/java/Main.java
@@ -0,0 +1,21 @@
+package com.example.java;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by I M O H on 6/24/2016.
+ *
+ */
+public class Main {
+    public static void main(String[] args){
+
+        List<String> list = new ArrayList<>();
+        list.add("California");
+        list.add("Orogon");
+        list.add("Washington");
+
+        list.add("Alaska");
+        System.out.println(list);
+    }
+}
